50ee00b8d6a07b624ffc769db01bf6ebcb7b7a27
ulfahalimah <ulfahalimah38@gmail.com>
2025-03-17 16:27:26 +0700

    [FEAT] submit table html challenges

---
+++ JS/main.js
@@ -0,0 +1,26 @@
+// console.log("halo batam");
+// document.write("saya senang sekali sebentar lagi libur")
+
+let nama = "ulfahalimah"; //camelCase
+console.log(nama);
+nama = "natasya";
+console.log(nama);
+
+let namaAja = "ulfa";
+console.log(namaAja);
+
+let ege = "17+"
+console.log(ege)z
+
+let isMale = false;
+console.log(typeof(isMale))
+
+let fruits = ["apple","orange","plum"]
+console.log(fruits[0]); // apple
+console.log(fruits[1]); // orange
+console.log(fruits[2]); // plum
+
+
+var nama1 
+
+// const nama2
